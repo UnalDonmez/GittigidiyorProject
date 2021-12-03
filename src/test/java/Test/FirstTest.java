c6ffefdde6460971c199d843d4f66900ca6030f4
UnalDonmez <79373213+UnalDonmez@users.noreply.github.com>
2021-12-03 22:32:55 +0300

    Update FirstTest.java

--- src/test/java/Test/FirstTest.java
+++ src/test/java/Test/FirstTest.java
@@ -35,7 +35,7 @@ public class FirstTest extends BaseTest {
         searchPage.SearchMyWord();
         searchPage.BackToHomePage();
         searchPage.SearchCanta();
-        //searchPage.searchCanta();
+        
 
 
     }
